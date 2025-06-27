1c63c0440212659ab6bea8a5a0ee6b74cb9f5112
Riley Fischer <rfmineguy@gmail.com>
2025-06-27 06:35:10 -0700

    Test: Add new file for the test support executable

---
+++ test/test_support.cpp
@@ -0,0 +1,4 @@
+#include "test_support.hpp"
+
+Support::Support(): x(4), y(3.1f) {}
+Support::~Support() {}
---
+++ test/test_support.hpp
@@ -0,0 +1,13 @@
+#ifndef TEST_SUPPORT_HPP
+#define TEST_SUPPORT_HPP
+
+class Support {
+  public:
+    Support();
+    ~Support();
+  private:
+    int x;
+    float y;
+};
+
+#endif
