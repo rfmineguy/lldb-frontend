f923af05e58e6c6c4d0f254aaf6ca39ff57b6a64
Riley Fischer <rfmineguy@gmail.com>
2025-06-27 06:27:12 -0700

    Util: Implement PrintModuleCompileUnits

--- src/Util.cpp
+++ src/Util.cpp
@@ -9,4 +9,26 @@ namespace Util {
       std::cout << "\t" << i << " : " << module.GetFileSpec().GetFilename() << std::endl;
     }
   }
+
+  void PrintModuleCompileUnits(lldb::SBTarget& target, int moduleIdx) {
+    uint32_t compileUnitCount = target.GetModuleAtIndex(0).GetNumCompileUnits();
+    std::cout << "Compile Unit Count: " << compileUnitCount << std::endl;
+
+    for (size_t i = 0; i < target.GetNumModules(); i++) {
+      auto module = target.GetModuleAtIndex(i);
+      if (!module.IsValid()) continue;
+
+      for (size_t j = 0; j < module.GetNumCompileUnits(); j++) {
+        auto compileUnit = module
+          .GetCompileUnitAtIndex(j);
+
+        lldb::SBFileSpec fs = compileUnit.GetFileSpec();
+        if (!fs.IsValid()) continue;
+        const char* path = fs.GetFilename();
+        if (path == NULL) continue;
+
+        std::cout << "FileSpec Path: " << path << std::endl;
+      }
+    }
+  }
 }
