0f6efe78a7522cfa818577a0fea188a435657631
Riley Fischer <rfmineguy@gmail.com>
2025-06-27 16:55:51 -0700

    FileHeirarchy: Implement free function

--- src/FileHeirarchy.cpp
+++ src/FileHeirarchy.cpp
@@ -3,4 +3,13 @@
 
 FileHeirarchy::FileHeirarchy(): mainRoot(new HeirarchyElement("/")) {}
 FileHeirarchy::~FileHeirarchy() {
+  Free(mainRoot);
+}
+
+void FileHeirarchy::Free(HeirarchyElement* element) {
+  for (auto& [key, child] : element->children) {
+    Free(child);
+  }
+  delete element;
+}
 }
