1c62d011cfff53b55dbf24d8df28272b8c5d6b9c
Riley Fischer <rfmineguy@gmail.com>
2025-06-27 06:26:45 -0700

    Util: Implement PrintTargetModules

--- src/Util.cpp
+++ src/Util.cpp
@@ -2,4 +2,11 @@
 #include <iostream>
 
 namespace Util {
+  void PrintTargetModules(lldb::SBTarget& target) {
+    std::cout << "TargetModules:" << std::endl;
+    for (int i = 0; i < target.GetNumModules(); i++) {
+      auto module = target.GetModuleAtIndex(i);
+      std::cout << "\t" << i << " : " << module.GetFileSpec().GetFilename() << std::endl;
+    }
+  }
 }
