39080904ab845bfc7c5e1046faf1d2925bcd5e7e
Riley Fischer <rfmineguy@gmail.com>
2025-06-29 12:25:38 -0700

    ImGuiCustomWidgets: Modified imgui breakpoint widget to reference the heirachy element

--- src/ImGuiCustomWidgets.hpp
+++ src/ImGuiCustomWidgets.hpp
@@ -1,12 +1,13 @@
 #ifndef CUSTOM_IMGUI_WIDGETS_HPP
 #define CUSTOM_IMGUI_WIDGETS_HPP
+#include "FileHeirarchy.hpp"
 
 struct Line;
 struct FileContext;
 struct ImGuiLayer;
 
 namespace ImGuiCustom {
-  void Breakpoint(int id, Line& line, FileContext& fctx, ImGuiLayer& imguiLayer);
+  void Breakpoint(int id, FileHeirarchy::HeirarchyElement& element, ImGuiLayer& imguiLayer);
 }
 
 #endif
