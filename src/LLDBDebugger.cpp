21c2c44459a1b397546eaae4f0e831c057150917
Riley Fischer <rfmineguy@gmail.com>
2025-06-29 14:04:09 -0700

    LLDBDebugger: Seperate WaitForEvent from the while condition. It was immediately ending the event loop like that.

--- src/LLDBDebugger.cpp
+++ src/LLDBDebugger.cpp
@@ -127,50 +127,52 @@ void LLDBDebugger::LLDBEventThread() {
   using namespace lldb;
   SBEvent event;
   bool running = true;
-  while (running && listener.WaitForEvent(1, event)) {
-    if (SBProcess::EventIsProcessEvent(event)) {
-      Logger::Info("Event name: {}", event.GetBroadcaster().GetName());
-      StateType state = SBProcess::GetStateFromEvent(event);
-      switch (state) {
-        case eStateStopped:
-          Logger::Info("Target stopped");
-          break;
-        case eStateExited:
-          Logger::Info("Target exited");
-          running = false;
-          break;
-        case eStateRunning:
-          Logger::Info("Target running");
-          break;
-        case eStateCrashed:
-          Logger::Info("Target crashed");
-          break;
-        case eStateInvalid:
-          Logger::Info("Invaid State");
-          break;
-        case eStateAttaching:
-          Logger::Info("Target attaching");
-          break;
-        case eStateConnected:
-          Logger::Info("Target connected");
-          break;
-        case eStateDetached:
-          Logger::Info("Target detatched");
-          break;
-        case eStateLaunching:
-          Logger::Info("Target launching");
-          break;
-        case eStateStepping:
-          Logger::Info("Target stepping");
-          break;
-        case eStateSuspended:
-          Logger::Info("Target suspended");
-          break;
-        case eStateUnloaded:
-          Logger::Info("Target unloaded");
-          break;
-        default:
-          break;
+  while (running) {
+    if (listener.WaitForEvent(1, event)) {
+      if (SBProcess::EventIsProcessEvent(event)) {
+        Logger::Info("Event name: {}", event.GetBroadcaster().GetName());
+        StateType state = SBProcess::GetStateFromEvent(event);
+        switch (state) {
+          case eStateStopped:
+            Logger::Info("Target stopped");
+            break;
+          case eStateExited:
+            Logger::Info("Target exited");
+            running = false;
+            break;
+          case eStateRunning:
+            Logger::Info("Target running");
+            break;
+          case eStateCrashed:
+            Logger::Info("Target crashed");
+            break;
+          case eStateInvalid:
+            Logger::Info("Invaid State");
+            break;
+          case eStateAttaching:
+            Logger::Info("Target attaching");
+            break;
+          case eStateConnected:
+            Logger::Info("Target connected");
+            break;
+          case eStateDetached:
+            Logger::Info("Target detatched");
+            break;
+          case eStateLaunching:
+            Logger::Info("Target launching");
+            break;
+          case eStateStepping:
+            Logger::Info("Target stepping");
+            break;
+          case eStateSuspended:
+            Logger::Info("Target suspended");
+            break;
+          case eStateUnloaded:
+            Logger::Info("Target unloaded");
+            break;
+          default:
+            break;
+        }
       }
     }
   }
