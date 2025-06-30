4479d670e2f76e9819778198d16bac8552181788
Riley Fischer <rfmineguy@gmail.com>
2025-06-29 20:58:05 -0700

    LLDBCommandParser: Implement parsing for breakpoints ('b main.cpp:4' and 'b main' style), next, step, continue, and run

--- src/LLDBCommandParser.cpp
+++ src/LLDBCommandParser.cpp
@@ -7,6 +7,44 @@ LLDB_CommandParser::LLDB_CommandParser() {}
 LLDB_CommandParser::~LLDB_CommandParser() {}
 
 LLDB_CommandParser::ParsedCommand LLDB_CommandParser::Parse(const std::string& command) {
+  std::vector<std::string> split = SplitBySpaces(command);
+  if (split.size() == 0) return LLDB_CommandParser::ParsedCommand{.type = ParsedCommandType::EMPTY};
+
+  // Parse short form first
+  //  b main.cpp:3
+  if (split.at(0) == "b") {
+    if (split.size() == 1) return LLDB_CommandParser::Invalid("Incomplete breakpoint command");
+    auto& where = split.at(1);
+    size_t colon = where.find(":");
+    if (colon != std::string::npos) { // b <file>:<line>
+      std::string file = where.substr(0, colon);
+      std::string line = where.substr(colon + 1);
+      try {
+        int line_int = std::stoi(std::string(line));
+        return ParsedCommand{.type = ParsedCommandType::BREAKPOINT_FILE_LINE, .command = BPFileLine(file, line_int)};
+      } catch (std::invalid_argument e) {
+        return LLDB_CommandParser::Invalid("Breakpoint line '{}' not an integer", line);
+      } catch (std::out_of_range e) {
+        return LLDB_CommandParser::Invalid("Breakpoint line '{}' out of range", line);
+      }
+    }
+    else { // b <symbol>
+      return ParsedCommand{.type = ParsedCommandType::BREAKPOINT_SYMBOL, .command = BPSymbol(where)};
+    }
+  }
+  if (split.size() == 1 && split.at(0) == "n") {
+    return ParsedCommand{.type = ParsedCommandType::NEXT};
+  }
+  if (split.size() == 1 && split.at(0) == "s") {
+    return ParsedCommand{.type = ParsedCommandType::STEP};
+  }
+  if (split.size() == 1 && split.at(0) == "c") {
+    return ParsedCommand{.type = ParsedCommandType::CONTINUE};
+  }
+  if (split.size() == 1 && split.at(0) == "r") {
+    return ParsedCommand{.type = ParsedCommandType::RUN};
+  }
+  return LLDB_CommandParser::Invalid("{} not valid", command);
 }
 
 std::vector<std::string> LLDB_CommandParser::SplitBySpaces(const std::string& s) {
