59edbc4cad112f59ba58591cb38ca3ec0ee298e4
Steven French <mr.steven.french@gmail.com>
2025-07-01 10:39:07 +1200

    test.cpp: print argv[0] for clarity

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-int main() {
+int main(int argc, const char **argv) {
+  std::cout << "argv[0]: " << argv[0] << std::endl;
   std::cout << "Hello world from test" << std::endl;
   std::cout << "Hello world from test" << std::endl;
   std::cout << "Hello world from test" << std::endl;
