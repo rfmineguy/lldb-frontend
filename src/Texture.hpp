0d64fbe5ae2b723bff6952c435a2a8ef8380b491
Riley Fischer <rfmineguy@gmail.com>
2025-07-01 16:18:14 -0700

    Texture: Setup basic interface for opengl textures

---
+++ src/Texture.hpp
@@ -0,0 +1,25 @@
+#ifndef TEXTURE_HPP
+#define TEXTURE_HPP
+#include <string>
+#include <glad/gl.h>
+struct ImVec2;
+
+class Texture {
+  public:
+    Texture(const std::string& local_path);
+    ~Texture();
+    unsigned int GetTextureId() const;
+    int GetWidth() const;
+    int GetHeight() const;
+    int GetChannels() const;
+    ImVec2 GetImGuiSizeScaled(float scaleFactor) const;
+  private:
+    void Load();
+  private:
+    bool isLoaded;
+    unsigned int textureId;
+    int width, height, channels;
+    std::string local_path, full_path;
+};
+
+#endif
