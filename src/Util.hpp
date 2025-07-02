12ee2c0f2f63c5d11907c38fd27aabab8705a83f
Riley Fischer <rfmineguy@gmail.com>
2025-07-02 10:53:26 -0700

    Util: Implemented ReadFileLinesIntoVector because its useful in multiple places

--- src/Util.hpp
+++ src/Util.hpp
@@ -4,12 +4,15 @@
 #include <filesystem>
 #include <optional>
 
+struct Line;
 namespace Util {
   void PrintTargetModules(lldb::SBTarget& target);
   void PrintModuleCompileUnits(lldb::SBTarget& target, int moduleIdx);
   std::filesystem::path GetCurrentProgramDirectory();
   std::string StringEscapeBackslash(const std::string& string);
   std::optional<std::filesystem::path> GetTargetSourceRootDirectory(std::filesystem::path start_dir);
+
+  bool ReadFileLinesIntoVector(const std::string& filepath, std::vector<Line>& lines);
 }
 
 #endif
