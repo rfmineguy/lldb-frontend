08b031ac0520c6a9946586d6500b126a0f006d40
Riley Fischer <rfmineguy@gmail.com>
2025-07-05 18:19:49 -0700

    ThemeObserver: Implemented windows theme change observer, and hooked it up to theme updater

--- src/Windows_ThemeListener.cpp
+++ src/Windows_ThemeListener.cpp
@@ -1,4 +1,32 @@
 #include "ThemeListener.hpp"
+#include "Util.hpp"
+#include <windows.h>
+#include <chrono>
+#define GLFW_EXPOSE_NATIVE_WIN32
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
+
+static WNDPROC g_OriginalWndProc = nullptr;
+static void (*g_ThemeChangeCallback)() = nullptr;
+
+static LRESULT CALLBACK ThemeAwareWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    static bool lastThemeDark = Util::GetSystemTheme() == Util::SystemTheme::DARK;
+
+    if ((msg == WM_THEMECHANGED || msg == WM_SETTINGCHANGE) && g_ThemeChangeCallback) {
+        bool currentDark = Util::GetSystemTheme() == Util::SystemTheme::DARK;
+        if (currentDark != lastThemeDark) {
+            lastThemeDark = currentDark;
+            g_ThemeChangeCallback();
+        }
+    }
+
+    return CallWindowProc(g_OriginalWndProc, hwnd, msg, wParam, lParam);
+}
 
 void RegisterThemeChangeObserver(void (*callback)()) {
+    GLFWwindow* curr = glfwGetCurrentContext();
+    HWND hwnd = glfwGetWin32Window(curr);
+    g_ThemeChangeCallback = callback;
+    g_OriginalWndProc = (WNDPROC)SetWindowLongPtr(hwnd, GWLP_WNDPROC, (LONG_PTR)ThemeAwareWndProc);
 }
